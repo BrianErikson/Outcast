a55d2ec78c5abd3aa7c487281ae03d135f78915c
BrianErikson <briankerikson@gmail.com>
2016-04-21 12:01:26 +0200

    added mediocre Atom support

--- src/main/com/beariksonstudios/outcast/RssManager.kt
+++ src/main/com/beariksonstudios/outcast/RssManager.kt
@@ -5,6 +5,7 @@ import com.sun.syndication.feed.synd.SyndEntryImpl
 import com.sun.syndication.feed.synd.SyndFeedImpl
 import com.sun.syndication.io.SyndFeedInput
 import com.sun.syndication.io.XmlReader
+import org.jdom.Element
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -13,6 +14,8 @@ import javax.json.Json
 import javax.json.JsonObject
 
 data class Feed(val title: String, val rssUrl: URL, val date: String);
+data class AtomFeed(val author: String, val subtitle: String, val summary: String, val explicit: String,
+                    val keywords: String, val owner: String, val category: String, val image: String);
 
 class RssManager() {
     private val dbFeedPath = URL("http://localhost/feeds");
@@ -54,12 +57,50 @@ class RssManager() {
             try {
                 val xmlReader = XmlReader(feed.rssUrl);
                 val rss = SyndFeedInput().build(xmlReader) as SyndFeedImpl;
-                return Podcast(feed.title, rss, if (rss.image != null) URL(rss.image.url) else null, rss.description);
+                var imageUrl: URL? = null;
+                if (rss.image != null) {
+                    imageUrl = URL(rss.image.url);
+                }
+                else {
+                    val atomFeed = parseAtomFeed(rss);
+                    if (atomFeed.image.isNotEmpty()) {
+                        imageUrl = URL(atomFeed.image);
+                    }
+                }
+                return Podcast(feed.title, rss, imageUrl, rss.description);
             }
             catch (e: IOException) {
                 e.printStackTrace();
                 return null;
             }
+        }
+
+        fun parseAtomFeed(rss: SyndFeedImpl): AtomFeed {
+            val elements: List<*> = rss.foreignMarkup as List<*>;
+            var author = "";
+            var subtitle = "";
+            var summary = "";
+            var explicit = "";
+            var keywords = "";
+            var owner = "";
+            var category = "";
+            var image = "";
+            elements.forEach {
+                if (it is Element) {
+                    when (it.name) {
+                        "author" -> author = it.value;
+                        "subtitle" -> subtitle = it.value;
+                        "summary" -> summary = it.value;
+                        "explicit" -> explicit = it.value;
+                        "keywords" -> keywords = it.value;
+                        "owner" -> owner = it.value;
+                        "category" -> category = it.value;
+                        "image" -> image = it.getAttributeValue("href");
+                    }
+                }
+            }
+
+            return AtomFeed(author, subtitle, summary, explicit, keywords, owner, category, image);
         }
 
         fun getTracks(podcast: Podcast): List<Track> {
